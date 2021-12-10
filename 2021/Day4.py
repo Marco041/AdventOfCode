b8dcd0a33f63963d93e91efabf8391649357a515
mrg1 <mrg1>
2021-12-10 21:00:46 +0100

    Day 4

---
+++ 2021/Day4.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+def parse_board_input(lines, line_size):
+    n_boards = len(lines) // line_size
+    boards = []
+    for i in range(n_boards):
+        for line in lines[line_size * i + 1 : line_size * (i + 1)]:
+            boards.append([int(num) for num in line.split()])
+    return boards
+
+def calc_winner(board_size, boards, draws):
+    score_list = []
+    excluded_board = dict()
+    for draw in draws:
+        for line_num, line in enumerate(boards):
+            for index, num in enumerate(line):
+                if(draw == num):
+                    boards[line_num][index] = 0
+                    winner_board_numer =  line_num//board_size
+                    index_board = winner_board_numer*board_size
+                    if(winner_board_numer not in excluded_board):
+                        col_sum = sum([x[index] for x in boards[index_board:index_board+board_size]])
+                        if(sum(boards[line_num]) == 0 or col_sum == 0):
+                            s = sum(sum(n) for n in boards[index_board:index_board+board_size])*draw
+                            score_list.append(s)
+                            excluded_board[winner_board_numer]=True
+    return (score_list[0], score_list[-1])
+
+fp = open("input4.txt")
+board_size = 5
+line_size = board_size+1
+draws = [int(x) for x in fp.readline()[:-1].split(",")]
+lines = fp.readlines()
+boards = parse_board_input(lines, line_size)
+result = calc_winner(board_size, boards, draws)
+print("Part 1: ", result[0])
+print("Part 2: ", result[1])
